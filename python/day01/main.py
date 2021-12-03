c252278654919ce4536637916b52c95ccb4488ad
sorachii <bencecube@gmail.com>
2021-12-04 00:49:04 +0100

    aoc_01: Added shebang and deleted unnecesary comments.

--- python/day01/main.py
+++ python/day01/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 with open("input", 'r') as f:
    scans = [int(line.strip()) for line in f.readlines()] 
 
@@ -8,16 +10,11 @@ for i, line in enumerate(scans):
     if i < len(scans) - 2:
         sum_of_windows.append(sum(scans[i:i+3]))
 
-
 for i, line in enumerate(scans):
     if i > 0 and line > scans[i-1]:
         counter += 1
     if  1998 > i > 0 and sum_of_windows[i] > sum_of_windows[i-1]:
         windows_counter += 1
 
-
-# Solution 1
 print(counter)
-
-# Solution 2
 print(windows_counter)
